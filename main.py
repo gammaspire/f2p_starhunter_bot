2723f3d9545fa70907835c83001eb5ce817ade19
Kyssuber <k22cong@siena.edu>
2025-08-13 14:30:52 -0500

    Fixing problem where bot was not outputting an error message if the user tried to hold a star with a tier < 6.

--- main.py
+++ main.py
@@ -454,52 +454,56 @@ async def hold(ctx, world=None, loc=None, tier=None):
     username = ctx.author.name
     user_id = ctx.author.id
     
-    #checks if world is f2p star; if not, goodbye.
+    #check if user remembered to include world, loc, and tier arguments
+    if not world or not loc or not tier:
+        await ctx.send(print_error_message(command='hold'))
+        await ctx.send('-# Come on, now. You should know better.')
+        return
+    
+    #now check if the world is f2p; if not, goodbye.
     if str(world) not in load_f2p_worlds():
         await ctx.send(print_error_message(command='hold'))
         await ctx.send('-# Use your noggin next time.')
         return
     
-    #load our location shorthand dictionary
-    loc_dict = load_loc_dict()
-    
-    #remove 't' or 'T' from the tier string, if any
-    tier = remove_frontal_corTex(tier)
-
+    #parse the tier...which must be between 6 and 9
     try:
-        
-        #if world_check returns TRUE, then the star is already in the JSON file!        
-        world_check_held = world_check_flag(world,filename='held_stars.json')
-                
-        #if there is already a star registered in the .json file in 'world', cancel the request
-        if world_check_held:
-            await ctx.send(f'There is already a star being held for world {world}.\nCheck the list of backup stars with the $backups command.')
+        tier = int(remove_frontal_corTex(tier))
+        if not (6 <= tier <= 9):
+            await ctx.send('Please make sure you are holding a star with a tier of at least 6.')
+            await ctx.send(f'-# You really wanted to hold a t{tier} star? Good heavens.')
             return
-        
-        world_check_active = world_check_flag(world, filename='active_stars.json')
-        #if there is already a star registered in the .json file in 'world', cancel the request
-        if world_check_active:
-            await ctx.send(f'There is already an active star for world {world}.\nCheck the list of active stars with the $active command.')
-            return
-        
-        #now compare wave time and eow suggested call time for star
-        #if call_flag = True, can call the star now; if call_flag = False, add star to file and hold
-        call_flag = check_wave_call(world,tier)   #will not run if call syntax is incorrect
-        if call_flag:
-            #view is what enables the button
-            await ctx.send(f"<⭐ {ctx.author.mention}> CALL STAR: World {world}, {loc}, Tier {tier}",
-                           view=CallStarView(username, user_id, world, loc, tier))  #CallStarView is a class
-            return
-                    
-    #TypeError --> user omitted world, loc, and/or tier from command
-    except TypeError:
+    except:
         await ctx.send(print_error_message(command='hold'))
         return
     
-    #if not call flag, the command was typed correctly, AND there is not already an entry with the
-    #world included in the command, add the held star to list and HOLD
-    add_star_to_list(username, user_id, world, loc, tier, filename='held_stars.json')  
-    await ctx.send(f"Holding the following ⭐:\nWorld: {world}\nLoc: {loc}\nTier: {tier}")
+    #check if the world is already held or called
+    #if so, cancel the request
+    if world_check_flag(world, filename='held_stars.json'):
+        await ctx.send(f'There is already a held star for world {world}.')
+        return
+    if world_check_flag(world, filename='active_stars.json'):
+        await ctx.send(f'There is already an active star for world {world}.')
+        return
+    
+    #load our location shorthand dictionary
+    loc_dict = load_loc_dict()
+    
+    #if it is indeed time to call the star, CALL THE STAR
+    #compares wave time and eow suggested call time for star
+    #if check_wave_call = True, can call the star now; if call_flag = False, add star to file and hold
+    if check_wave_call(world, tier):
+        await ctx.send(f"<⭐ {ctx.author.mention}> CALL STAR: World {world}, {loc}, Tier {tier}",
+                       view=CallStarView(username, user_id, world, loc, tier))   #CallStarView is a class
+        return
+    
+    #lastly...if none of the above catches terminated the function, hold the star~
+    #...and then proceed to the scheduler functions below
+    try:
+        add_star_to_list(username, user_id, world, loc, tier, filename='held_stars.json')
+        await ctx.send(f"Holding the following ⭐:\nWorld: {world}\nLoc: {loc}\nTier: {tier}")
+    except Exception as e:
+        await ctx.send(f"Failed to hold star due to the following error: {e}")
     
     #schedule the checking job; if star is ready to call, remove job!
     async def monitor_star():
@@ -529,22 +533,14 @@ async def hold(ctx, world=None, loc=None, tier=None):
     def run_job():
         asyncio.run_coroutine_threadsafe(monitor_star(), bot.loop)
 
+    #THE FUNCTIONS DEFINED ABOVE WILL RUN WITH THE LAST FEW LINES
     #unique job ID (based on user + star details)
     job_id = f"hold_{world}_{tier}"
     
     #will run run_job every two minutes!
     if not scheduler.get_job(job_id):
         scheduler.add_job(run_job, 'interval', minutes=2, id=job_id)
-    
 
-@hold.error
-async def call_error(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send(print_error_message(command='hold'))
-        await ctx.send('-# Use your noggin next time.')
-    else:
-        raise error
-    
 ############################################################
 #print list of current backup stars in an aesthetic textbox
 #use: 
@@ -703,7 +699,7 @@ async def start_active_loop(ctx,minutes=10):
     #associates server ID with the channel ID
     scheduled_channel_ids_active[guild_id] = channel_id
 
-    await ctx.send(f"Active stars will be posted in this channel every {minutes} minute(s)!")
+    await ctx.send(f"The Active Stars post will be updated in this channel every {minutes} minute(s)!")
     
     #NOTE: message_id=123 is a PLACEHOLDER. If I do not define it at all, the default is message_id=None and the
     #code will not output a message_id (which I need for $start_active_loop)
