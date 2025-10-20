959c4c614c220d6e2fe14eb7eb15aa6ff9c08c29
Kyssuber <k22cong@siena.edu>
2025-10-20 14:35:21 -0500

    tidying up main.py, creating foundation for testing the writing of poof data to dust.wiki

--- main.py
+++ main.py
@@ -1,47 +1,29 @@
-import discord
+from discord import Intents
 import os
 from discord.ext import commands
 import asyncio
 import sys
 
-from pull_f2p_worlds import pull_f2p_worlds
-
 sys.path.append('utils')
 from scheduler_utils import scheduler, init_scheduler_jobs, reset_star_jsons
 from button_utils import restore_hoplist_view
+from onready_utils import load_cogs, sync_commands
+from pull_f2p_worlds import pull_f2p_worlds
 
 sys.path.append('config')
 from config import TOKEN, GUILD
 
-
 ################################################################################
 ################################################################################
 ################################################################################
-
-guild = GUILD
 
 #define intents; required to read message content in on_message() (see events/sad_ears.py)
-intents = discord.Intents.default()
+intents = Intents.default()
 intents.members = True   #need to detect when new members join :-)
 intents.message_content = True
 
 #create bot instance (inherits from discord.Client)
 bot = commands.Bot(command_prefix="$", intents=intents, help_command=None)   #I am creating my own help_command
-
-################################################################################
-#loading the COGS!
-#each Cog is a bot command or event! this function simply loads them for use.
-################################################################################
-async def load_cogs():
-    for folder, prefix in [("./commands", "commands"), ("./events", "events")]:
-        for filename in os.listdir(folder):
-            if filename.endswith(".py") and filename != "__init__.py":
-                #in /commands, I have __init__.py, so python treats /commands as a package
-                #every .py file is its own Class, just like numpy.array or scipy.stats
-                #as such, I load the extension as commands._____, with the [:-3] removing the '.py'
-                ext_name = f"{prefix}.{filename[:-3]}"
-                if ext_name not in bot.extensions:
-                    await bot.load_extension(ext_name)
 
 ################################################################################
 ################################################################################
@@ -72,24 +54,13 @@ async def on_ready():
     await restore_hoplist_view(bot)
     
     #add the Cogs!
-    await load_cogs()
+    await load_cogs(bot)
     
-    #### Syncing slash commands
-    #this will sync the commands with Discord's API.
-    #it is recommended to do this only once or when you change commands.
-    #if you have a lot of commands, you might want to use `sync(guild_id=GUILD_ID)` to sync only for a specific guild.
-    #replace GUILD_ID with your actual guild ID if you want to limit the sync.
-    #for global commands, you can use `bot.tree.sync()` without any parameters.
-    #note: Global commands can take up to an hour to propagate.  
-    try:
-        synced = await bot.tree.sync(guild=guild)
-        print(f"Synced {len(synced)} slash commands.")
-    except Exception as e:
-        print(f"Error syncing commands: {e}")
-
+    #syncing slash commands
+    await sync_commands(bot, GUILD)
+    
 ################################################################################
 ################################################################################
 ################################################################################    
-    
     
 bot.run(TOKEN)
---
+++ utils/onready_utils.py
@@ -0,0 +1,55 @@
+'''
+Various utility functions that will run when the bot activates!
+'''
+
+################################################################################
+#loading the COGS!
+#each Cog is a bot command or event! this function simply loads them for use.
+################################################################################
+async def load_cogs(bot):
+    import os
+    for folder, prefix in [("./commands", "commands"), ("./events", "events")]:
+        for filename in os.listdir(folder):
+            if filename.endswith(".py") and filename != "__init__.py":
+                #in /commands, I have __init__.py, so python treats /commands as a package
+                #every .py file is its own Class, just like numpy.array or scipy.stats
+                #as such, I load the extension as commands._____, with the [:-3] removing the '.py'
+                ext_name = f"{prefix}.{filename[:-3]}"
+                if ext_name not in bot.extensions:
+                    await bot.load_extension(ext_name)
+
+
+################################################################################
+#syncing the slash commands!
+#each Cog is a bot command or event! this function simply loads them for use.
+################################################################################
+async def sync_commands(bot, guild):
+    #this will sync the commands with Discord's API.
+    #it is recommended to do this only once or when you change commands.
+    #if you have a lot of commands, you might want to use `sync(guild_id=GUILD_ID)` to sync only for a specific guild.
+    #replace GUILD_ID with your actual guild ID if you want to limit the sync.
+    #for global commands, you can use `bot.tree.sync()` without any parameters.
+    #note: Global commands *can* take up to an hour to propagate.  
+    try:
+        synced = await bot.tree.sync(guild=guild)   #if guild=None, then sync is global
+                                                    #for Tester Bot, I specify a guild.
+        print(f"Synced {len(synced)} slash commands.")
+    except Exception as e:
+        print(f"Error syncing commands: {e}")
+                    
+                    
+################################################################################
+#creating and sending the onready message!
+#this is only applicable if I am running Tester Bot.
+################################################################################
+async def onready_message(bot, ctx_id):
+
+    channel = bot.get_channel(ctx_id)
+
+    if channel:
+        await channel.send("A FRIENDLY REMINDER!\n\n" 
+                           "As the Tester Bot, I am reading from and writing to [this Google Sheet](<https://docs.google.com/spreadsheets/d/1i52-v3hCBZlFQong3EFyHZtc3rP47njUEDsWAV6_FoU/edit?gid=930734666#gid=930734666>), the testing equivalent to [loon.dust.wiki](<https://docs.google.com/spreadsheets/d/1mNLGnXrlr4v-8dzM-4xQp7YpMq4D34Dnaaengs-jXeo/edit?gid=0#gid=0>).\n\n"
+                          "I am also using a different Google Service Account to my esteemed colleague, F2P Starhunter (known affectionately as bot-kun). See notes.txt for further details.\n\n"
+                           "-# If you do not know what notes.txt refers to, this message was not meant for you. Cope.")
+    else:
+        print(f"Error: Channel with ID {channel} not found.")
